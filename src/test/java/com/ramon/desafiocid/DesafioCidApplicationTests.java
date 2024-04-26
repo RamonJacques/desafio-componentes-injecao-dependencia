7f8f7b1fd985cd17ab0bc58277d72ed1008953e7
RamonJacques <ramonjacquesrs@gmail.com>
2024-04-26 10:46:31 -0300

    Project created

---
+++ src/test/java/com/ramon/desafiocid/DesafioCidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.desafiocid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
